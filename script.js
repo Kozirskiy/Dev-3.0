8d47b40ca3dc497005ebbc63cbbca12dc85e79f7
Olex <example@i.ua>
2022-12-31 10:21:39 +0200

    first commit dev 3.0

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//alert('sdgsdg');
+
+//const ask = confirm('are you here?');
+//console.log(ask);
+
+// const ask = +prompt('do you have 18 years ?', 'please enter here "yes" or "no"');
+
+// console.log(typeof(ask));
+
+const answers = [];
+
+// answers[0] = prompt('what is your name?', '');
+// answers[1] = prompt('what is your second name?', '');
+// answers[2] = prompt('what is your third name?', '');
+
+
+console.log(typeof(answers));
