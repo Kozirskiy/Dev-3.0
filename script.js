f789c34dba478020ded6b9828489b3c9f18f34cf
Olex <example@i.ua>
2023-01-01 10:54:16 +0200

    first consol's spa

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 'use strict';
 
-//alert('sdgsdg');
-
-//const ask = confirm('are you here?');
-//console.log(ask);
-
-// const ask = +prompt('do you have 18 years ?', 'please enter here "yes" or "no"');
-
-// console.log(typeof(ask));
-
-const answers = [];
-
-// answers[0] = prompt('what is your name?', '');
-// answers[1] = prompt('what is your second name?', '');
-// answers[2] = prompt('what is your third name?', '');
 
 
-console.log(typeof(answers));
+const numberOfFilms = +prompt('How many movies have you watched?', '');
+
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('one of the last movies watched?', ''),
+      b = prompt('How much do you rate it?', ''),
+      c = prompt('one of the last movies watched?', ''),
+      d = prompt('How much do you rate it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
