b85ebac98a735b221b758adec54987f8a4034072
Olex <example@i.ua>
2023-01-06 15:27:07 +0200

    exercices

--- script.js
+++ script.js
@@ -5,25 +5,117 @@
 // } else {
 //     console.log('no ok');
 // }
-let num = 57;
+// let num = 57;
 
 // (num ===50) ? console.log('ok') : console.log('not ok'); 
 
-switch (num) {
-    case 49: 
-        console.log('Wrong');
-        break;
-    case 56: 
-        console.log("It's more than to need");
-        break;
-    case 55: 
-        console.log("Exectly");
-        break;
-    default:   
-        console.log('you must find more');
-        break;
+// switch (num) {
+//     case 49: 
+//         console.log('Wrong');
+//         break;
+//     case 56: 
+//         console.log("It's more than to need");
+//         break;
+//     case 55: 
+//         console.log("Exectly");
+//         break;
+//     default:   
+//         console.log('you must find more');
+//         break;
+// }
+
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 5; j++) {
+//         console.log(j);
+//     }
+// }
+
+let result = '';
+const lenght = 7;
+
+for (let i = 1; i < lenght; i++) {
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+    result += '\n';
 }
 
+console.log(result);
+
+
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if ( k === 2 ) break first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
+
+
+
+// function fifthTask() {
+//     const arrayOfNumbers = [];
+
+//     for (let i = 5; i < 11; i++) {
+//         arrayOfNumbers[i - 5] = i;
+//     }
+
+//     console.log(arrayOfNumbers);
+//     return arrayOfNumbers;
+// }
+
+// fifthTask();
+
+
+// function firstTask() {
+//     const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         result[i] = arr[i];
+//     }
+
+//     console.log(result);
+//     return result;
+// }
+
+// firstTask();
+
+// function secondTask() {
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+//     for (let i = 0; i < data.length; i++) {
+//         if (typeof(data[i]) === 'number') {
+//             data[i] = data[i] * 2;
+//         } else if (typeof(data[i]) === 'string') {
+//             data[i] = `${data[i]} - done`;
+//         }
+//     }
+
+//     console.log(data);
+//     return data;
+// }
+
+// secondTask();
+
+function thirdTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = 1; i <= data.length; i++) {
+        result[i - 1] = data[data.length - i]
+    }
+
+    console.log(result);
+    return result;
+}
+
+thirdTask();
 
 
 //_______________________________________________________________________________________________
