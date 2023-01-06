098868baecba69e0f5ec377d3aecec4d18da55fd
Olex <example@i.ua>
2023-01-06 07:37:30 +0200

    netx comm

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 'use strict';
 
+// if ( 5 == 6 ) {
+//     console.log('ok');
+// } else {
+//     console.log('no ok');
+// }
+let num = 57;
+
+// (num ===50) ? console.log('ok') : console.log('not ok'); 
+
+switch (num) {
+    case 49: 
+        console.log('Wrong');
+        break;
+    case 56: 
+        console.log("It's more than to need");
+        break;
+    case 55: 
+        console.log("Exectly");
+        break;
+    default:   
+        console.log('you must find more');
+        break;
+}
+
+
+
+//_______________________________________________________________________________________________
 
 
 const numberOfFilms = +prompt('How many movies have you watched?', '');
